ef4713438a436ea27132331617701812a823730b
KARIM7856 <36071198+KARIM7856@users.noreply.github.com>
2020-12-30 21:15:12 +0200

    .

---
+++ src/Login.java
@@ -0,0 +1,62 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("Password");
+		
+		UserT u = new UserT(username, password);
+		
+		Configuration con = new Configuration().configure().addAnnotatedClass(UserT.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session sess = sf.openSession();
+		Transaction t = sess.beginTransaction();
+		
+		
+		
+		sess.save(u);
+		
+		t.commit();
+		
+		response.getWriter().println("SSSS");
+	}
+
+}
